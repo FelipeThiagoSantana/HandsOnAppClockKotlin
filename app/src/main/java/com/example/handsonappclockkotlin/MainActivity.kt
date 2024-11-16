28898ab4b6e28d3fcb03a8c585675019f185627a
FelipeThiagoSantana <107277318+FelipeThiagoSantana@users.noreply.github.com>
2024-11-16 12:00:13 -0300

    I finished the app, creating functions to get the current time. I set the android manifest to the portraint position.

--- app/src/main/java/com/example/handsonappclockkotlin/MainActivity.kt
+++ app/src/main/java/com/example/handsonappclockkotlin/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.handsonappclockkotlin
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextClock
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var txtHoraAtual: TextClock? = null
+    private var textHora24AM_PM: TextClock? =null
+    private var txtUpdateHoraAtual: TextView? = null
+    private var btnUpdateHoraAtual: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        iniciarComponentesDeLayout()
     }
+
+    private fun iniciarComponentesDeLayout() {
+        txtHoraAtual = findViewById(R.id.txtHoraAtual)
+        textHora24AM_PM = findViewById(R.id.txtHora24PM_AM)
+        txtUpdateHoraAtual = findViewById(R.id.txtUpdateHoraAtual)
+        btnUpdateHoraAtual = findViewById(R.id.btnUpdateHoraAtual)
+    }
+
+    fun AtualizarHora(view: View) {
+        txtUpdateHoraAtual!!.setText("Hora Atual "+ textHora24AM_PM!!.getText())
+    }
+
+
+
+
+
+
 }
